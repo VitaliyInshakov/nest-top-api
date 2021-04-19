302eeb142ed9890a6d086d69dd5d233035fa29ae
Vitaliy Inshakov <michael8913@gmail.com>
2021-04-19 23:29:55 +0300

    custom pipe

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -15,6 +15,7 @@ import { CreatReviewDto } from "./dto/creat-review.dto";
 import { ReviewService } from "./review.service";
 import { REVIEW_NOT_FOUND } from "./review.constants";
 import { JwtGuard } from "../auth/guards/jwt.guard";
+import { IdValidationPipe } from "../pipes/id-validation.pipe";
 
 @Controller('review')
 export class ReviewController {
@@ -28,7 +29,7 @@ export class ReviewController {
 
     @UseGuards(JwtGuard)
     @Delete(':id')
-    async delete(@Param('id') id: string) {
+    async delete(@Param('id', IdValidationPipe) id: string) {
         const deletedDoc = await this.reviewService.delete(id);
         if (!deletedDoc) {
             throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
@@ -36,7 +37,7 @@ export class ReviewController {
     }
 
     @Get('byProduct/:productId')
-    async getByProduct(@Param('productId') productId: string) {
+    async getByProduct(@Param('productId', IdValidationPipe) productId: string) {
         return this.reviewService.findByProductId(productId);
     }
 }
