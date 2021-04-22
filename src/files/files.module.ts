67e0b6471afb49cdc28c36e089f8b37e07239846
Vitaliy Inshakov <michael8913@gmail.com>
2021-04-22 23:30:42 +0300

    convert images to webP

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { path } from "app-root-path";
+import { ServeStaticModule } from "@nestjs/serve-static";
+
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
 
 @Module({
+  imports: [ServeStaticModule.forRoot({
+    rootPath: `${path}/uploads`,
+    serveRoot: "/static",
+  })],
   controllers: [FilesController],
   providers: [FilesService]
 })
