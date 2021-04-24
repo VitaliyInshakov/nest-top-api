b906e2e82d1cb9b4e3d21720cd282a13163b9f3f
Vitaliy Inshakov <michael8913@gmail.com>
2021-04-24 22:39:09 +0300

    telegram bot integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,8 @@ import { ProductModule } from "./product/product.module";
 import { ReviewModule } from "./review/review.module";
 import { getMongoConfig } from "./configs/mongo.config";
 import { FilesModule } from './files/files.module';
+import { TelegramModule } from './telegram/telegram.module';
+import { getTelegramConfig } from "./configs/telegram.config";
 
 @Module({
   imports: [
@@ -23,7 +25,12 @@ import { FilesModule } from './files/files.module';
     TopPageModule,
     ProductModule,
     ReviewModule,
-    FilesModule
+    FilesModule,
+    TelegramModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getTelegramConfig,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -16,15 +16,30 @@ import { ReviewService } from "./review.service";
 import { REVIEW_NOT_FOUND } from "./review.constants";
 import { JwtGuard } from "../auth/guards/jwt.guard";
 import { IdValidationPipe } from "../pipes/id-validation.pipe";
+import { TelegramService } from "../telegram/telegram.service";
 
 @Controller('review')
 export class ReviewController {
-    constructor(private readonly reviewService: ReviewService) {}
+    constructor(
+        private readonly reviewService: ReviewService,
+        private readonly telegramService: TelegramService,
+    ) {}
 
     @UsePipes(new ValidationPipe())
     @Post('create')
     async create(@Body() dto: CreatReviewDto) {
        return this.reviewService.create(dto);
+    }
+
+    @UsePipes(new ValidationPipe())
+    @Post('notify')
+    async notify(@Body() dto: CreatReviewDto) {
+        const message = `Name: ${dto.name}\n`
+            + `Title: ${dto.title}\n`
+            + `Description: ${dto.description}\n`
+            + `Rating: ${dto.rating}\n`
+            + `Product ID: ${dto.productId}\n`;
+        return this.telegramService.sendMessage(message);
     }
 
     @UseGuards(JwtGuard)
